c204a570d241f9a9f12a887fd5d7e3f41f3eb848
Emma Linso <em@linso.online>
2026-04-01 20:14:19 -0600

    Add post pages

--- app/newsletter/page.tsx
+++ app/newsletter/page.tsx
@@ -1,7 +1,31 @@
-export default function Page() {
+import Link from "next/link";
+import { type SanityDocument } from "next-sanity";
+
+import { client } from "@/app/sanity/client";
+
+const POSTS_QUERY = `*[
+  _type == "post"
+  && defined(slug.current)
+]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
+
+const options = { next: { revalidate: 30 } };
+
+export default async function IndexPage() {
+	const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
+
 	return (
-		<main>
-			<p>Newsletter page</p>
+		<main className="container mx-auto min-h-screen max-w-3xl p-8">
+			<h1 className="text-4xl font-bold mb-8">Posts</h1>
+			<ul className="flex flex-col gap-y-4">
+				{posts.map((post) => (
+					<li className="hover:underline" key={post._id}>
+						<Link href={`newsletter/${post.slug.current}`}>
+							<h2 className="text-xl font-semibold">{post.title}</h2>
+							<p>{new Date(post.publishedAt).toLocaleDateString()}</p>
+						</Link>
+					</li>
+				))}
+			</ul>
 		</main>
 	);
 }
