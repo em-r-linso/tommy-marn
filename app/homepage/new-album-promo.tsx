30a022c2685e883634ccd670cef27951e7fdafe0
Emma Linso <em@linso.online>
2026-04-21 20:18:38 -0600

    Add new album promo

---
+++ app/homepage/new-album-promo.tsx
@@ -0,0 +1,66 @@
+import Link from "next/link";
+import { createImageUrlBuilder } from "@sanity/image-url";
+import type { SanityImageSource } from "@sanity/image-url";
+import { client } from "@/app/sanity/client";
+
+const { projectId, dataset } = client.config();
+
+const urlFor = (source: SanityImageSource) => {
+	if (!projectId || !dataset) return null;
+	return createImageUrlBuilder({ projectId, dataset }).image(source);
+};
+
+type Album = {
+	title: string;
+	slug: { current: string };
+	coverImage: SanityImageSource;
+};
+
+export default function NewAlbumPromo({ album }: { album?: Album }) {
+	if (!album) return null;
+
+	return (
+		<section className="group relative block w-full aspect-[20/9]">
+			<img
+				src="/new-album-promo/drum.svg"
+				className="absolute left-[6%] bottom-[0%] w-[14%] h-auto"
+			/>
+			<img
+				src="/new-album-promo/egg.svg"
+				className="absolute right-[0%] top-[0%] w-[2%] h-auto"
+			/>
+			<img
+				src="/new-album-promo/guitar-amp-hat.svg"
+				className="absolute right-[0%] bottom-[0%] w-[45%] h-auto"
+			/>
+			<img
+				src="/new-album-promo/guitar.svg"
+				className="absolute left-[5%] top-[5%] w-[25%] h-auto"
+			/>
+			<img
+				src="/new-album-promo/heart.svg"
+				className="absolute bottom-[0%] bottom-[0%] w-[2%] h-auto"
+			/>
+			<img
+				src="/new-album-promo/synth-laptop.svg"
+				className="absolute left-[0%] top-[0%] w-[46%] h-auto"
+			/>
+			<img
+				src="/new-album-promo/xylophone.svg"
+				className="absolute right-[12%] top-[0%] w-[18%] h-auto"
+			/>
+			<Link
+				href={`/music/${album.slug.current}`}
+				className="absolute left-[50%] -translate-x-1/2 top-0 w-[36%] h-[100%]"
+				style={{ containerType: "inline-size" }} // non-tailwind style because cqw not supported in tailwind
+			>
+				<img src={urlFor(album.coverImage)?.url()} alt={album.title} />
+				<span className="w-full block text-center" style={{ fontSize: "14cqw" }}>
+					{" "}
+					{/* non-tailwind style because cqw not supported in tailwind */}
+					Listen Now!
+				</span>
+			</Link>
+		</section>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
 import TarotPromo from "./homepage/tarot-promo";
 import Portraits from "./homepage/portraits";
+import NewAlbumPromo from "./homepage/new-album-promo";
+import { client } from "@/app/sanity/client";
 
-export default function Page() {
+const PROMO_ALBUM_SLUG = "the-queen-of-time";
+
+const ALBUM_QUERY = `*[_type == "album" && slug.current == $slug][0]{
+	title,
+	coverImage,
+	slug { current }
+}`;
+
+export default async function Page() {
+	const album = await client.fetch(ALBUM_QUERY, { slug: PROMO_ALBUM_SLUG });
+
 	return (
 		<main className="flex flex-col items-center gap-4">
 			<TarotPromo />
+			<NewAlbumPromo album={album} />
 			<Portraits />
 		</main>
 	);
