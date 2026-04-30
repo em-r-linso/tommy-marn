c2b0634be5fb0334554ec34577d310080f90eb42
Emma Linso <em@linso.online>
2026-04-30 15:03:01 -0600

    Add test for testing branch

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,7 @@ export default async function Page() {
 
 	return (
 		<main className="flex flex-col items-center gap-15">
+			Test :)
 			<TarotPromo />
 			<NewAlbumPromo album={album} />
 			<PatreonPromo />
