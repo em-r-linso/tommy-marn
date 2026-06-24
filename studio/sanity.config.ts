37fd780173c70ec37255f3f52e02c8c3e93d9c56
Emma Linso <em@linso.online>
2026-06-23 19:11:50 -0600

    Fix sanity.io sync

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,18 +1,30 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemaTypes";
 
 export default defineConfig({
-  name: 'default',
-  title: 'tommymarn.com',
+	name: "default",
+	title: "tommymarn.com",
 
-  projectId: 'ye87psy7',
-  dataset: 'production',
+	projectId: "ye87psy7",
+	dataset: "production",
 
-  plugins: [structureTool(), visionTool()],
+	plugins: [
+		structureTool({
+			structure: (S) =>
+				S.list()
+					.title("Content")
+					.items([
+						S.documentTypeListItem("post").title("Posts"),
+						S.documentTypeListItem("album").title("Albums"),
+						S.documentTypeListItem("tourDate").title("Tour Dates"),
+					]),
+		}),
+		visionTool(),
+	],
 
-  schema: {
-    types: schemaTypes,
-  },
-})
+	schema: {
+		types: schemaTypes,
+	},
+});
